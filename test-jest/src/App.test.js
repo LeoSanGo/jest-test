bd512e164a4dc3040f97ad06a3cf0c8195ef71e1
leosango <souoleo@gmail.com>
2022-04-22 16:18:04 -0300

    Third suit of tests

--- test-jest/src/App.test.js
+++ test-jest/src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 import AccountBalance from './components/AccountBalance';
 import Notification from './components/Notification';
 import { shallow, mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
 
 const userBalance = {
   balance: 1100,
@@ -50,5 +51,44 @@ describe('passing props', () => {
   });
   it('notification accepts props', () => {
     expect(notificationWrapper.props().balance).toEqual(userBalance.balance);
+  });
+});
+
+describe('logic', () => {
+  const wrapper = mount(<AccountBalance accounts={userBalance} />);
+  const notificationWrapper = mount(
+    <Notification balance={userBalance.balance} />
+  );
+  wrapper.find('#balance-button').simulate('click');
+  it('button click - update savings', () => {
+    const savingsValue = wrapper.find('.savings').text();
+    const expectedValue = userBalance.savingsBalance + 100 + '$';
+    expect(savingsValue).toEqual(expectedValue);
+  });
+  it('button click - update balance', () => {
+    const balanceValue = wrapper.find('.balance').text();
+    const expectedBalanceValue =
+      userBalance.balance -
+      100 +
+      expect(balanceValue).toEqual(expectedBalanceValue);
+  });
+});
+
+describe('snapshots', () => {
+  it('App snapshots', () => {
+    const tree = shallow(<App />);
+    expect(toJson(tree)).toMatchSnapshot();
+  });
+  it('Accounts snapshot', () => {
+    const accountBalanceTree = shallow(
+      <AccountBalance accounts={userBalance} />
+    );
+    expect(toJson(accountBalanceTree)).toMatchSnapshot();
+  });
+  it('Notification snapshot', () => {
+    const notificationTree = shallow(
+      <Notification balance={userBalance.balance} />
+    );
+    expect(toJson(notificationTree)).toMatchSnapshot();
   });
 });
